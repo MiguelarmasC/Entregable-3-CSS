3cd3339b8f00dcd14b0c047e33d5daa0c66c4e44
Miguel Armas <armax.96@gmail.com>
2026-04-12 10:06:38 +0200

    configuracion del viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/Entregable-3-CSS/", 
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contacto: resolve(__dirname, 'map_page.html'),
+      },
+    },
+  },
 });
